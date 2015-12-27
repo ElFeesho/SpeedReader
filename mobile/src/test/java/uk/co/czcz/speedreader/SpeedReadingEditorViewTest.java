29a9f30d3668eed90e65a50dc2a154134c03f9bd
Chris Sawczuk <gummybassist@gmail.com>
2015-12-27 21:52:01 +0000

    SpeedReadingEditorPresenter tests passing.

--- mobile/src/test/java/uk/co/czcz/speedreader/SpeedReadingEditorViewTest.java
+++ mobile/src/test/java/uk/co/czcz/speedreader/SpeedReadingEditorViewTest.java
@@ -19,33 +19,129 @@ public class SpeedReadingEditorViewTest {
         assertThat(spySpeedReadingLauncherDelegate.launchLocally_called, is(false));
     }
 
+    @Test
+    public void whenASpeedReadingEditorViewIsEmpty_launchingASpeedReadingSessionOnWearIsNotPossible() {
+
+        FakeSpeedReadingEditorView fakeSpeedReadingEditorView = new FakeSpeedReadingEditorView();
+        SpySpeedReadingLauncherDelegate spySpeedReadingLauncherDelegate = new SpySpeedReadingLauncherDelegate();
+        new SpeedReadingEditorPresenter(fakeSpeedReadingEditorView, spySpeedReadingLauncherDelegate);
+
+        fakeSpeedReadingEditorView.fake_launchSpeedReadingOnWear();
+
+        assertThat(spySpeedReadingLauncherDelegate.launchWear_called, is(false));
+    }
+
+    @Test
+    public void whenASpeedReadingEditorViewContainsText_ASpeedReadingSessionCanBeLaunchedLocallyWithTheEnteredText() {
+
+        String expectedText = "Expected Text";
+        FakeSpeedReadingEditorView fakeSpeedReadingEditorView = FakeSpeedReadingEditorView.withEnteredText(expectedText);
+        SpySpeedReadingLauncherDelegate spySpeedReadingLauncherDelegate = new SpySpeedReadingLauncherDelegate();
+        new SpeedReadingEditorPresenter(fakeSpeedReadingEditorView, spySpeedReadingLauncherDelegate);
+
+        fakeSpeedReadingEditorView.fake_launchSpeedReadingLocally();
+
+        assertThat(spySpeedReadingLauncherDelegate.launchLocally_called, is(true));
+        assertThat(spySpeedReadingLauncherDelegate.launchLocally_text, is(expectedText));
+    }
+
+    @Test
+    public void whenASpeedReadingEditorViewContainsText_ASpeedReadingSessionCanBeLaunchedOnWearWithTheEnteredText() {
+
+        String expectedText = "Expected Text";
+        FakeSpeedReadingEditorView fakeSpeedReadingEditorView = FakeSpeedReadingEditorView.withEnteredText(expectedText);
+        SpySpeedReadingLauncherDelegate spySpeedReadingLauncherDelegate = new SpySpeedReadingLauncherDelegate();
+        new SpeedReadingEditorPresenter(fakeSpeedReadingEditorView, spySpeedReadingLauncherDelegate);
+
+        fakeSpeedReadingEditorView.fake_launchSpeedReadingOnWear();
+
+        assertThat(spySpeedReadingLauncherDelegate.launchWear_called, is(true));
+        assertThat(spySpeedReadingLauncherDelegate.launchWear_text, is(expectedText));
+    }
+
     public static class SpeedReadingEditorPresenter {
 
         private final View view;
         private final SpeedReadingLauncherDelegate speedReadingLauncherDelegate;
 
-        public interface View
-        {
+        public interface View {
+            interface Listener {
+                void requestLaunchLocally(String text);
+
+                void requestLaunchWear(String text);
+            }
+
+            void addListener(Listener listener);
         }
 
-        public interface SpeedReadingLauncherDelegate
-        {
+        public interface SpeedReadingLauncherDelegate {
+            void launchLocally(String text);
 
+            void launchWear(String text);
         }
 
-        public SpeedReadingEditorPresenter(View view, SpeedReadingLauncherDelegate speedReadingLauncherDelegate) {
+        public SpeedReadingEditorPresenter(View view, final SpeedReadingLauncherDelegate speedReadingLauncherDelegate) {
             this.view = view;
             this.speedReadingLauncherDelegate = speedReadingLauncherDelegate;
+
+            view.addListener(new View.Listener() {
+                @Override
+                public void requestLaunchLocally(String text) {
+                    if (text.length() > 0) {
+                        speedReadingLauncherDelegate.launchLocally(text);
+                    }
+                }
+
+                @Override
+                public void requestLaunchWear(String text) {
+                    if (text.length() > 0) {
+                        speedReadingLauncherDelegate.launchWear(text);
+                    }
+                }
+            });
         }
     }
 
     private static class FakeSpeedReadingEditorView implements SpeedReadingEditorPresenter.View {
-        public void fake_launchSpeedReadingLocally() {
+        private String editorText = "";
+        private Listener listener;
 
+        public void fake_launchSpeedReadingLocally() {
+            listener.requestLaunchLocally(editorText);
+        }
+
+        public static FakeSpeedReadingEditorView withEnteredText(String editorText) {
+            FakeSpeedReadingEditorView fakeSpeedReadingEditorView = new FakeSpeedReadingEditorView();
+            fakeSpeedReadingEditorView.editorText = editorText;
+            return fakeSpeedReadingEditorView;
+        }
+
+        @Override
+        public void addListener(Listener listener) {
+            this.listener = listener;
+        }
+
+        public void fake_launchSpeedReadingOnWear() {
+            listener.requestLaunchWear(editorText);
         }
     }
 
     private class SpySpeedReadingLauncherDelegate implements SpeedReadingEditorPresenter.SpeedReadingLauncherDelegate {
         public boolean launchLocally_called;
+        public String launchLocally_text;
+        public boolean launchWear_called;
+        public String launchWear_text;
+
+        @Override
+        public void launchLocally(String text) {
+            launchLocally_called = true;
+            launchLocally_text = text;
+        }
+
+        @Override
+        public void launchWear(String text) {
+            launchWear_called = true;
+            launchWear_text = text;
+        }
     }
 }
